510706d36a0b30dbe6ed76573b61fe5a8164f96a
marzzy16 <114106672+marzzy16@users.noreply.github.com>
2023-03-01 13:54:55 -0600

    Initial commit

---
+++ Strings_Sawyer_Moira/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Strings_Sawyer_Moira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Askes user to input a character and assigns input to a variable
+            Console.WriteLine("Please input a character!");
+            char myChar = Console.ReadKey().KeyChar;
+
+            // Checks if the character user entered is a letter
+            bool isletter = Char.IsLetter(myChar);
+
+            // Prints the results to console
+            Console.WriteLine($"\n{myChar} is a letter: {isletter}.");
+
+             // Asks user a question and saves response to a variable, sentence
+            Console.WriteLine("Why do you like video games?");
+            string sentece = Console.ReadLine();
+
+           // Asks user to enter a word, saves response to a veriable called word
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+
+            // Checks if the word is in the sentence
+            bool isIn = sentece.Contains(word);
+
+            // Prints if the word is in the sentence
+            Console.WriteLine($"{word} exists in {sentece}, {isIn}!");
+
+
+        }
+    }
+}
